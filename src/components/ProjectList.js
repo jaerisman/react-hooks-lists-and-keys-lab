409056ced7954a5c5d1f5831060595eadd6080af
Jacob Erisman <jacob.erisman@gmail.com>
2023-10-24 02:50:56 -0400

    lab complete

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import user from "../data/user";
 
 function ProjectList({ projects }) {
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem 
+            key={project.id} 
+            name={project.name} 
+            project={project}
+            technologies={project.technologies} 
+          />
+        ))}
+      </div>
     </div>
   );
 }
